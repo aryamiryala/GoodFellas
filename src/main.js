b764a30d605715eb7aa3dfeb66be5bb6eff334c9
Arya Miryala <aryamiryala5@gmail.com>
2023-05-30 17:28:44 -0700

    menu

---
+++ src/main.js
@@ -0,0 +1,24 @@
+let config = {
+    type: Phaser.CANVAS, 
+    width: 640, 
+    height: 480,
+    physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+          //debug: true
+          
+        }
+
+    },
+    scene: [Menu]
+}
+
+let game = new Phaser.Game(config);
+
+//reserve keyboard vars
+let keyUP, keyDOWN, keyLEFT, keyRIGHT, keyI, keyR, keyM; 
+
+//set UI sizes
+let borderUISize = game.config.height / 15; 
+let borderPadding = borderUISize / 3; 
